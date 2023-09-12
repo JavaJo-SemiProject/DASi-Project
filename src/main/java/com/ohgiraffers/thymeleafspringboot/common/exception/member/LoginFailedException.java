4415c74631079d348f5cc988c6a50aa12436aed1
김민범 <minbum0501@naver.com>
2023-09-12 19:34:29 +0900

    파일 리팩토링

--- src/main/java/com/ohgiraffers/thymeleafspringboot/common/exception/member/LoginFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.thymeleafspringboot.common.exception.member;
-
-public class LoginFailedException extends Exception {
-
-    public LoginFailedException(String msg){
-        super(msg);
-    }
-
-}
