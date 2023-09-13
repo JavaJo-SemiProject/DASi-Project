435aa063c4a358ab9c5ab82d0ae346aa2c8798e0
vaguewords <vaguewords@gmail.net>
2023-09-13 18:54:49 +0900

    회원 db 조회 및 등록 (진행중)
    회원 db 조회 및 등록 (진행중)

--- dasi/src/main/java/com/project/dasi/admin/memberInfo/controller/AdminController.java
+++ dasi/src/main/java/com/project/dasi/admin/memberInfo/controller/AdminController.java
@@ -1,34 +1,18 @@
 package com.project.dasi.admin.memberInfo.controller;
-import com.project.dasi.admin.memberInfo.dto.UserInfoDTO;
-import com.project.dasi.admin.memberInfo.service.UserInfoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin/memberInfo")
 public class AdminController {
     @GetMapping("/admin")
     public void goAdmin() {}
-    @GetMapping("/userList")
-    public void goUserList() {}
-    private final UserInfoService userInfoService;
 
-    public AdminController(UserInfoService userInfoService) {
-        this.userInfoService = userInfoService;
-    }
+    /*@GetMapping("/memberList")
+    public void goMemberList() {}
 
-    public String selectUserList(Model model) {
-
-        List<UserInfoDTO> userList = userInfoService.selectAllUserInfo();
-
-        model.addAttribute("userList", userList);
-
-        return "/memberInfo/userList";
-
-    }
+    @GetMapping("/memberRegist")
+    public void goMemberRegist() {}*/
 
 }
