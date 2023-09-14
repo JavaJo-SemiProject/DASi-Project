5a10cb950c57456a9d12e5c9e535f114140c58fd
Gahyeon <dididi1993@hanmail.net>
2023-09-14 19:35:45 +0900

    커밋

--- src/main/java/com/project/dasi/configuration/MyBatisConfiguration.java
+++ src/main/java/com/project/dasi/configuration/MyBatisConfiguration.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
 @Configuration
-@MapperScan(basePackages = "com.project.dasi", annotationClass = Mapper.class)
-public class MybatisConfiguration {}
+@MapperScan(basePackages = "com.project.dasi", annotationClass = Mapper.class) {
+public void MybatisConfiguration () {}
+        }*/
--- src/main/java/com/project/dasi/member/faq/controller/FaqController.java
+++ src/main/java/com/project/dasi/member/faq/controller/FaqController.java
@@ -1,29 +1,31 @@
 package com.project.dasi.member.faq.controller;
 
 import com.project.dasi.member.faq.dto.FaqDTO;
-import com.project.dasi.member.faq.service.FaqServiceImpl;
+import com.project.dasi.member.faq.service.FaqService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/member/faq/*")  /*@RequestMapping과 @GetMapping은 같다 */
+@RequestMapping("/member/..")  /*@RequestMapping과 @GetMapping은 같다 */
 public class FaqController {
 
-    private final FaqServiceImpl faqService;
+    private final FaqService faqService;
 
-    public FaqController(FaqServiceImpl faqService) {this.faqService = faqService;}
-/*여기에 오토와이어드 어노테이션이 자동으로 달린다. 컨트롤러 안에 있는 단 하나의 주입이니까*/
+    public FaqController(FaqService faqService) {this.faqService = faqService;}
+    /*여기에 오토와이어드 어노테이션이 자동으로 달린다. 컨트롤러 안에 있는 단 하나의 주입이니까*/
 
-    @GetMapping ("/faq")
-    public ModelAndView faqList(ModelAndView mv) {
+    @GetMapping ("/faq/faq")
+    public String faqList(Model model) {
         List<FaqDTO> faqList = faqService.selectAllFaqList();
-        mv.addObject("faqList", faqList);
-        mv.setViewName("member/faq/faq");
-        return mv;
+        model.addAttribute("faqList", faqList);
+        return "/member/faq/faq";   /*뷰단에 보여주는거라서 여기서 작성한주소는 templete하위의 것들이다*/
+
     }
+
 }
 
