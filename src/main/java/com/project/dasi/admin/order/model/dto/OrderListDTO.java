9378c07d41d0c6b221cddadd5ae22f8996b24ce2
costudying <bnj021212@gmail.com>
2023-09-15 19:09:35 +0900

    주문 관련 기능 0.1.2

--- src/main/java/com/project/dasi/admin/order/model/dto/OrderListDTO.java
+++ src/main/java/com/project/dasi/admin/order/model/dto/OrderListDTO.java
@@ -1,5 +1,8 @@
-package com.project.dasi.admin.orderList.dto;
+package com.project.dasi.admin.order.model.dto;
+
 import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -8,14 +11,14 @@ import lombok.*;
 @ToString
 public class OrderListDTO implements java.io.Serializable {
     private String orderId;
-    private String productCode;
-    private String statusNum;
+    private List<ProdctDTO> productCode;
+    private List<OrderStatusDTO> statusNum;
     private String payNum;
     private String cancelContent;
     private String sizeInfo;
     private String phrase;
     private String reqIssues;
-    private String orderTime;
     private String orderDate;
     private String orderPrice;
+    private List<PayInfoDTO> payCode;
 }
---
+++ src/main/java/com/project/dasi/admin/order/model/dto/ProdctCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.project.dasi.admin.order.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProdctCategoryDTO {
+
+    private String categoryCode;
+    private String categoryName;
+
+}
