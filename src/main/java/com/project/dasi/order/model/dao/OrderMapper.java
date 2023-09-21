e597bab9b88c4632958070a0a3cb9557c86b6c27
costudying <bnj021212@gmail.com>
2023-09-21 09:43:47 +0900

    주문 제작 기능 수정 0.5.1

--- src/main/java/com/project/dasi/order/model/dao/OrderMapper.java
+++ src/main/java/com/project/dasi/order/model/dao/OrderMapper.java
@@ -1,11 +1,15 @@
-package com.project.dasi.member.order.dao;
+package com.project.dasi.order.model.dao;
 
-import com.project.dasi.member.order.dto.OrderListDTO;
+import com.project.dasi.order.model.dto.OrderFileDTO;
+import com.project.dasi.order.model.dto.OrderListDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface OrderMapper {
-    List<OrderListDTO> findMyAllOrder();
+
+    int insertOrderContent(OrderListDTO order);
+
+    int insertOrderFile(OrderFileDTO orderFileDTO);
 }
--- src/main/java/com/project/dasi/order/model/service/OrderService.java
+++ src/main/java/com/project/dasi/order/model/service/OrderService.java
@@ -1,7 +1,15 @@
-package com.project.dasi.member.order.service;
+package com.project.dasi.order.model.service;
+
+import com.project.dasi.order.model.dto.OrderListDTO;
+
+import java.util.List;
 
 public interface OrderService {
 
-    public int selectOrderList();
+    public List<OrderListDTO> selectMyOrderList();
+
+    public OrderListDTO selectMyOrderDetail(String userId);
+
+    public void registOrder(OrderListDTO orderList);
 
 }
