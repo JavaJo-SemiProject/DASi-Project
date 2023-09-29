fd9d7d020aab34488268cda8afa3b0e8fad36661
vaguewords <vaguewords@gmail.net>
2023-09-29 22:35:54 +0900

    09/29 commit

--- src/main/java/com/project/dasi/configuration/WebSecurityConfig.java
+++ src/main/java/com/project/dasi/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.project.dasi.configuration;
 
 import com.project.dasi.auth.model.service.UserService;
+import com.project.dasi.auth.controller.AuthFailController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebSecurityConfig {
     private final UserService userService;
 
+    private final AuthFailController authFailController;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -44,7 +47,7 @@ public class WebSecurityConfig {
             http.formLogin()
                 .loginPage("/content/member/login")
                 .defaultSuccessUrl("/")
-                //.failureHandler(authFailHandler)
+                .failureHandler(authFailController)
                 .usernameParameter("userId")
                 .passwordParameter("password")
                 .and()
